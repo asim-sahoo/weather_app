1ed9d34d83d259efe07e2dd47c3d1dcfb82c6744
asimgeek <asim2018off@gmail.com>
2024-02-10 22:48:03 +0530

    minor bug fixed

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -2,8 +2,6 @@ class Weather {
   final String cityName;
   final double temperature;
   final String mainCondition;
-  final int sunrise; // Unix timestamp for sunrise time
-  final int sunset; // Unix timestamp for sunset time
   final double precipitation; // Precipitation in mm
   final int humidity; // Humidity percentage
   final double windSpeed; // Wind speed in m/s
@@ -12,8 +10,6 @@ class Weather {
     required this.cityName,
     required this.temperature,
     required this.mainCondition,
-    required this.sunrise,
-    required this.sunset,
     required this.precipitation,
     required this.humidity,
     required this.windSpeed,
@@ -24,8 +20,6 @@ class Weather {
       cityName: json['name'],
       temperature: double.parse(json['main']['temp'].toStringAsFixed(1)),
       mainCondition: json['weather'][0]['main'],
-      sunrise: json['sys']['sunrise'],
-      sunset: json['sys']['sunset'],
       precipitation: json['rain'] != null ? json['rain']['1h'].toDouble() : 0.0,
       humidity: json['main']['humidity'],
       windSpeed: json['wind']['speed'].toDouble(),
